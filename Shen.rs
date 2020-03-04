1b674b7a89ba19382370328d4e63f2b01be77483
Alexey <nafiguspofigus@gmail.com>
2020-03-04 17:20:24 +0300

    Update Shen.rs

--- Shen.rs
+++ Shen.rs
@@ -20,3 +20,49 @@ fn main()
     x = x - y;
     println!("x: {}, y: {}", x, y);
 }
+
+
+// Shen' - 1.1.4
+fn main()
+{
+    println!("{}", function_loop(5, 1, 3));
+}
+
+fn function_loop(mut k: i32, mut b: i32, mut c: i32) -> i32
+{
+    let mut count_of_steps: i32 = 0;
+    while k != 0
+    {
+        if k % 2 == 0
+        {
+            k = k / 2;
+            c = c * c;
+        }
+        else
+        {
+            k = k - 1;
+            b = b * c;
+        }
+        count_of_steps += 1;
+    }
+    count_of_steps
+}
+
+// Shen' - 1.1.5
+
+// a, b => a * b use (+, -, =, !=)
+
+fn main()
+{
+    println!("{}", calculator_a_b(5, 5));
+}
+
+fn calculator_a_b(a: u32, b: u32) -> u32
+{
+    let mut k = 0u32;
+    for i in 0..b
+    {
+        k += a;
+    }
+    k
+}
